a5bc656ef7599c125244b74aa66d1d0e72da3b90
Wilsons-Navid <wadotiwawil@gmail.com>
2024-06-25 08:42:49 +0000

    exporting to csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-    script returns TODO progress
-"""
+""""Module"""
+
+import csv
 import json
 import requests
 from sys import argv
@@ -14,13 +14,13 @@ if __name__ == "__main__":
     request_employee = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
     """
-        convert json to dictionary
+        json to dictionary
     """
-    employee = json.loads(request_employee.text)
+    user = json.loads(request_employee.text)
     """
-        extract employee name
+        extract username
     """
-    employee_name = employee.get("name")
+    username = user.get("username")
 
     """
         request user's TODO list
@@ -32,23 +32,19 @@ if __name__ == "__main__":
     """
     tasks = {}
     """
-        convert json to list
+        json to list
     """
-    employee_todos = json.loads(request_todos.text)
+    user_todos = json.loads(request_todos.text)
     """
         get completed tasks
     """
-    for dictionary in employee_todos:
+    for dictionary in user_todos:
         tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
     """
-        return name and progress
+        export to CSV
     """
-    EMPLOYEE_NAME = employee_name
-    TOTAL_NUMBER_OF_TASKS = len(tasks)
-    NUMBER_OF_DONE_TASKS = len([k for k, v in tasks.items() if v is True])
-    print("Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    for k, v in tasks.items():
-        if v is True:
-            print("\t {}".format(k))
+    with open('{}.csv'.format(argv[1]), mode='w') as file:
+        file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+        for k, v in tasks.items():
+            file_editor.writerow([argv[1], username, v, k])
